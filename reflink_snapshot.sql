1d633fa20df336453bc5d2453ab894a02926c902
Brian Ristuccia <brianr@insiderscore.com>
2026-01-16 14:00:17 -0500

    keep track of where we got our snapshot from

--- reflink_snapshot.sql
+++ reflink_snapshot.sql
@@ -1,4 +1,23 @@
 -- reflink_snapshot.sql
+-- Security definer function to validate backup_label
+CREATE OR REPLACE FUNCTION validate_backup_label(backup_label text)
+    RETURNS void
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    AS $$
+BEGIN
+    IF backup_label IS NULL OR length(trim(backup_label)) = 0 THEN
+        RAISE EXCEPTION 'backup_label cannot be null or empty';
+    END IF;
+    IF backup_label !~ '[a-zA-Z]' THEN
+        RAISE EXCEPTION 'backup_label must contain at least one alphabetic character';
+    END IF;
+    IF backup_label ~ '/' THEN
+        RAISE EXCEPTION 'backup_label cannot contain slashes';
+    END IF;
+END;
+$$;
+
 -- Security definer function to create a reflink snapshot
 CREATE OR REPLACE FUNCTION reflink_snapshot(backup_label text)
     RETURNS text
@@ -10,6 +29,8 @@ DECLARE
     dest_prefix text := current_setting('reflink.dest_prefix');
     snapshot_path text;
 BEGIN
+    PERFORM
+        validate_backup_label(backup_label);
     snapshot_path := dest_prefix || '/' || backup_label;
     -- Start backup
     PERFORM
@@ -19,7 +40,26 @@ BEGIN
     -- Stop backup and write label into the snapshot directory
     EXECUTE format('COPY (SELECT labelfile FROM pg_backup_stop(FALSE)) TO PROGRAM %L WITH csv', 'head -c -2 | tail -c +2 | tee "' || snapshot_path || '/backup_label"');
     -- Return the full filesystem path to the new snapshot
-    RETURN snapshot_path;
+    RETURN backup_label;
+END;
+$$;
+
+-- delete_snapshot.sql
+-- Security definer function to delete a reflink snapshot
+CREATE OR REPLACE FUNCTION delete_snapshot(backup_label text)
+    RETURNS void
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+    AS $$
+DECLARE
+    dest_prefix text := current_setting('reflink.dest_prefix');
+    snapshot_path text;
+BEGIN
+    PERFORM
+        validate_backup_label(backup_label);
+    snapshot_path := dest_prefix || '/' || backup_label;
+    -- Delete the snapshot directory
+    EXECUTE format('COPY (SELECT 1) TO PROGRAM %L', 'rm -rf "' || snapshot_path || '"');
 END;
 $$;
 
